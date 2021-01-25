332d32d8b7d2dd3793553a3f9d3c40ac270e2f1b
kumol <kumol01763@gmail.com>
2021-01-26 03:36:50 +0600

    dg

--- src/App.vue
+++ src/App.vue
@@ -1,66 +1,27 @@
 <template>
-  <div id="nav">
-      <nav class="navbar navbar-default">
-  <div class="container-fluid">
-    <!-- Brand and toggle get grouped for better mobile display -->
-    <div class="navbar-header">
-      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-        <span class="sr-only">Toggle navigation</span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
+  <div class="nav-border">
+    <nav class="navbar navbar-expand-md">
+      <a class="navbar-brand" href="#">Navbar</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
       </button>
-      <a class="navbar-brand" href="#">Brand</a>
-    </div>
-
-    <!-- Collect the nav links, forms, and other content for toggling -->
-    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-      <ul class="nav navbar-nav">
-        <li><router-link to="/">Home</router-link> </li>
-        <li><router-link to="/doctor">Doctor </router-link></li>
-        <li><router-link to="/add">Add New Doctor</router-link></li>
-        <li><router-link to="/doctor">Doctor </router-link></li>
-        <li><router-link to="/organization/new">New Organization </router-link></li>
-        <li><router-link to="/organization">Organization </router-link></li>
-        <li class="dropdown">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
-          <ul class="dropdown-menu">
-            <li><a href="#">Action</a></li>
-            <li><a href="#">Another action</a></li>
-            <li><a href="#">Something else here</a></li>
-            <li role="separator" class="divider"></li>
-            <li><a href="#">Separated link</a></li>
-            <li role="separator" class="divider"></li>
-            <li><a href="#">One more separated link</a></li>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav">
+            <li class="nav-item active"><router-link class="nav-link nav-title" to="/">Home</router-link> </li>
+            <li class="nav-item"><router-link class="nav-link nav-title" to="/doctor">Doctor </router-link></li>
+            <li class="nav-item"><router-link class="nav-link nav-title" to="/add">Add New Doctor</router-link></li>
+            <li class="nav-item"><router-link class="nav-link nav-title" to="/doctor">Doctor </router-link></li>
+            <li class="nav-item"><router-link class="nav-link nav-title" to="/organization/new">New Organization </router-link></li>
+            <li class="nav-item"><router-link class="nav-link nav-title" to="/organization">Organization </router-link></li>
+            
           </ul>
-        </li>
-      </ul>
-      <form class="navbar-form navbar-left">
-        <div class="form-group">
-          <input type="text" class="form-control" placeholder="Search">
         </div>
-        <button type="submit" class="btn btn-default">Submit</button>
-      </form>
-      <ul class="nav navbar-nav navbar-right">
-        <li><a href="#">Link</a></li>
-        <li class="dropdown">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
-          <ul class="dropdown-menu">
-            <li><a href="#">Action</a></li>
-            <li><a href="#">Another action</a></li>
-            <li><a href="#">Something else here</a></li>
-            <li role="separator" class="divider"></li>
-            <li><a href="#">Separated link</a></li>
-          </ul>
-        </li>
-      </ul>
-    </div><!-- /.navbar-collapse -->
-  </div><!-- /.container-fluid -->
-</nav>
 
-      
+    </nav>
   </div>
-  <router-view />
+  <div class="body">
+    <router-view />
+  </div>
 </template>
 
 <script>
@@ -77,5 +38,50 @@ export default {
 <style>
   .m-0{
     margin: 0px !important;
+  }
+  .input:focus {
+    outline: none !important;
+    border:1px solid #eeeeee;
+    box-shadow: 0 0 2px #2195F3; 
+  }
+
+  .input{
+    border-radius: 4px 4px;
+    border: 1px solid #eeeeee!important;
+    padding: 5px 10px;
+  }
+  button:focus{
+    outline: none !important;
+    
+  }
+  .nav-border{
+    border-bottom: 2px solid #2195F3;
+  }
+
+  .body{
+    background-color: #fbfbfb;
+  }
+
+  .nav-title{
+    color:#2195F3 im !important;
+  }
+  .notch{
+    width: 0;
+    height: 0;
+    border-left: 15px solid transparent;
+    border-right: 15px solid transparent;
+    border-bottom: 15px solid #eeeeee;
+    margin-top: -23px;
+    margin-left: 150px;
+  }
+  .custom-menu{
+    top: 15px !important;
+    border: 1px solid #eeeeee !important;
+    box-shadow: 1px 1px 15px #eeeeee !important;
+    border-radius: 5px !important;
+  }
+
+  .doctor-find-menu{
+    width: 300px !important;
   }
 </style>
--- src/components/Layout/Home.vue
+++ src/components/Layout/Home.vue
@@ -1,18 +1,40 @@
 <template>
-    <div class="row">
+    <div class="row" style="margin:0px">
         <div class="col-md-6">
 
         </div>
-        <div class="col-md-6">
+        <div class="col-md-6" style="padding:50px">
             <form>
                 
 
-            <div class="form-group">
-                <label for="doctor">Select Doctor</label>
+            <!-- <div class="form-group">
+                
                 <select v-model="doctor" class="form-control" id="doctor">
                     <option v-for="doc in doctors" :key="doc._id" v-bind:value="{ _id: doc._id }">{{doc.name}}</option>
                 </select>
+            </div> -->
+
+            <div class="dropdown  form-group">
+                <label for="doctor">Select Doctor</label>
+                <input type="text" v-model="doctorName" placeholder="Type Doctor Name" class="input dropdown-toggle" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                
+                <div class="dropdown-menu doctor-find-menu custom-menu" aria-labelledby="dropdownMenuLink">
+                   <div class="notch"></div>
+                    <a v-for="doc in filteredCommands" :key="doc._id" @click="selectDoctor($event,doc)" class="dropdown-item">{{doc.name}}</a>
+                </div>
             </div>
+
+            <!-- <div class="dropdown">
+                <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Dropdown button
+                </button>
+                <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                    <a class="dropdown-item" href="#">Action</a>
+                    <a class="dropdown-item" href="#">Another action</a>
+                    <a class="dropdown-item" href="#">Something else here</a>
+                </div>
+            </div> -->
+            
             <div>
                 <label for="doctor">Select Date</label>
                 <input class="form-control" v-model="date" type="date" placeholder="date input">
@@ -31,6 +53,7 @@ export default {
         return{
             doctors:[],
             doctor:null,
+            doctorName:"",
             user:"",
             date:Date,
             time:Date,
@@ -54,6 +77,20 @@ export default {
         submitData(event){
             event.preventDefault();
             console.log(this.doctor._id);
+        },
+        selectDoctor(event,doctor){
+            event.preventDefault();
+            this.doctor = doctor._id;
+            this.doctorName = doctor.name;
+            console.log(doctor);
+        }
+    },
+    computed: {
+        filteredCommands: function() {
+            var matcher = new RegExp(this.doctorName, 'i')
+            return this.doctors.filter(function(doctor){
+                return matcher.test(doctor.name)
+            })
         }
     },
     mounted(){
