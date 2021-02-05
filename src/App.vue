6531ed850d295416470d50ebdd4a1f3100886fa3
kumol <kumol01763@gmail.com>
2021-02-05 23:28:59 +0600

    login user

--- src/App.vue
+++ src/App.vue
@@ -5,54 +5,57 @@
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav">
-            <li class="nav-item active"><router-link class="nav-link nav-title" to="/">Home</router-link> </li>
-            <li class="nav-item"><router-link class="nav-link nav-title" to="/doctor">Doctor </router-link></li>
-            <li class="nav-item"><router-link class="nav-link nav-title" to="/add">Add New Doctor</router-link></li>
-            <li class="nav-item"><router-link class="nav-link nav-title" to="/doctor">Doctor </router-link></li>
-            <li class="nav-item"><router-link class="nav-link nav-title" to="/organization/new">New Organization </router-link></li>
-            <li class="nav-item"><router-link class="nav-link nav-title" to="/organization">Organization </router-link></li>
-            
-          </ul>
-        </div>
-
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav">
+          <li class="nav-item active"><router-link class="nav-link nav-title" to="/">Home</router-link> </li>
+          <li class="nav-item"><router-link class="nav-link nav-title" to="/doctor">Doctor </router-link></li>
+          <li class="nav-item"><router-link class="nav-link nav-title" to="/add">Add New Doctor</router-link></li>
+          <li class="nav-item"><router-link class="nav-link nav-title" to="/doctor">Doctor </router-link></li>
+          <li class="nav-item"><router-link class="nav-link nav-title" to="/organization/new">New Organization </router-link></li>
+          <li class="nav-item"><router-link class="nav-link nav-title" to="/organization">Organization </router-link></li>
+        </ul>
+      </div>
     </nav>
     <div class="col-md-4 col-lg-3 signupdiv">
       <nav>
         <ul class="signuplogin">
           <li class="dropdown"><button class="button2 mr-10" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">SignUp</button>
             <div class="dropdown-menu signupmenu" aria-labelledby="dropdownMenuLink">
-                        <div class="signupnotch"></div>
-                        <div>
-                          <form class="form-group">
-                            <label for="number">Number</label>
-                            <input type="text" :v-model="number" name="number" class="input" id=""/>
-                          </form>
-                          <div>
-                            <button @click="logIn()" >Login</button>
-                          </div>
-                        </div>
-                    </div>
+                <div class="signupnotch"></div>
+              <form>
+                <div class="form-group">
+                  <label for="name">Name</label>
+                  <input type="text" v-model="name" class="form-control input" id="fullName"  placeholder="Enter Full Name">
+
+                  <!-- <input type="text" v-model="name" class="input" id=""/> -->
+                </div>
+                <div class="form-group">
+                  <label for="number">Number</label>
+                  <input type="text" v-model="number"  class="input" id=""/>
+                </div>
+              </form>
+              <div>
+                <button @click="join($event)" class="button" >Join</button>
+                <p>{{name}}</p>
+                <p>{{number}}</p>
+              </div>
+            </div>
           </li>
           <li class="dropdown"> <button class="button2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">LogIn</button>
             <div class="dropdown-menu signupmenu" aria-labelledby="dropdownMenuLink">
               <div class="signupnotch"></div>
-              <form>
-                <div class="form-group">
-                  <label for="name">Name</label>
-                  <input type="text" :v-model="name" name="name" class="input" id=""/>
-                </div>
-                <div class="form-group">
-                  <label for="number">Number</label>
-                  <input type="text" :v-model="number" name="number" class="input" id=""/>
-                </div>
-              </form>
               <div>
-                <button @click="logIn()" >Login</button>
+                <form class="form-group">
+                  <label for="number">Number</label>
+                  <input type="text" v-model="number" name="number" class="input" id=""/>
+                </form>
+                <div>
+                  <button class="button" @click="logIn($event)" >Login</button>
+                </div>
               </div>
             </div>
           </li>
+          <li>{{name}}</li>
         </ul>
       </nav>
     </div>
@@ -69,26 +72,42 @@
 import Footer from "./components/Layout/Footer"
 
 export default {
-  number:"",
-  name:"",
+  
   components: {
     Footer
   },
+  data(){
+    return{
+      number:"",
+      name:"",
+    }
+  },
   methods:{
-    logIn(event){
-      event.preventDefault();
+    async logIn(event){
+      event.preventDefault()
+      console.log("go");
       fetch("http://localhost:8000/api/user/login",{
-                method: "POST",
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body:this.number
-            }).then((response)=>{
-                console.log(response);
-            })
+          method: "POST",
+          headers: {
+              'Content-Type': 'application/json'
+          },
+          body:JSON.stringify({number:this.number})
+      }).then((response)=>{
+        //localStorage.setItem("user",JSON.stringify(response));
+        //console.log(response);
+        return response.json();
+      }).then(json=>{
+        localStorage.setItem("user",JSON.stringify(json));
+        let appUser = localStorage.getItem('user');
+        console.log(json);
+        console.log(appUser);
+      }).catch(err=>{
+        console.log(err);
+      })
     },
     join(event){
       event.preventDefault();
+      console.log(this.name + " " + this.number);
       fetch("http://localhost:8000/api/user/save",{
         method:"POST",
         headers:{
@@ -96,7 +115,10 @@ export default {
         },
         body:JSON.stringify({name:this.name,number:this.number,accountType:5})
       }).then((response)=>{
+        console.log(response.status);
         console.log(response);
+      }).catch(err=>{
+        console.log(err);
       })
     }
   }
