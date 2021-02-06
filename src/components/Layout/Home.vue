75e9f3b7b0dacd8cc5e4d5db878eade1bb2cb81d
kumol <kumol01763@gmail.com>
2021-02-06 23:32:51 +0600

    sds

--- src/components/Layout/Home.vue
+++ src/components/Layout/Home.vue
@@ -5,7 +5,16 @@
         </div>
         <div class="col-md-6" style="padding:50px">
             <form>
-                
+                <div class="dropdown  form-group">
+                    <label for="doctor">Select Doctor</label>
+                    <input type="text" v-model="orgName" placeholder="Type Organization Name" class="input form-control dropdown-toggle" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    
+                    <div class="dropdown-menu doctor-find-menu custom-menu" aria-labelledby="dropdownMenuLink">
+                        <div class="notch1"></div>
+                        <a v-for="org in filteredOrganization" :key="org._id" @click="selectOrg($event,org)" class="dropdown-item">{{org.name}}</a>
+                    </div>
+                </div>
+
                 <div class="dropdown  form-group">
                     <label for="doctor">Select Doctor</label>
                     <input type="text" v-model="doctorName" placeholder="Type Doctor Name" class="input form-control dropdown-toggle" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
@@ -31,10 +40,10 @@
                 </div>
             </div> -->
             
-            <div>
+            <!-- <div>
                 <label for="doctor">Select Date</label>
                 <input class="form-control" v-model="date" type="date" placeholder="date input">
-            </div>
+            </div> -->
 
             <button class="button mr-10 mt-10">Go</button>
             <button class="button1 mr-10 mt-10">demo</button>
@@ -46,12 +55,16 @@
     </div>
 </template>
 <script>
-import axios from "axios"
+import axios from "axios";
+//const configapi = require("../../../public/config.json").development.api;
 export default {
     name:"Home",
     data(){
         return{
             doctors:[],
+            organizations:[],
+            organization:null,
+            orgName:"",
             doctor:null,
             doctorName:"",
             user:"",
@@ -74,6 +87,17 @@ export default {
                 console.log(response);
             });
         },
+        getAllOrganization(){
+            axios.get("http://localhost:8000/api/organization/").then((response)=>{
+                if(response.status == 200){
+                    this.organizations = response.data.result;
+                    console.log("success");
+                    console.log(this.organizations);
+                }else{
+                    console.log("error occured");
+                }
+            });
+        },
         submitData(event){
             event.preventDefault();
             console.log(this.doctor._id);
@@ -83,18 +107,33 @@ export default {
             this.doctor = doctor._id;
             this.doctorName = doctor.name;
             console.log(doctor);
+        },
+        selectOrg(event,org){
+            event.preventDefault();
+            console.log("selected ")
+            this.organization = org._id;
+            this.orgName = org.name;
+            console.log(org);
         }
     },
     computed: {
         filteredCommands: function() {
             var matcher = new RegExp(this.doctorName, 'i')
             return this.doctors.filter(function(doctor){
-                return matcher.test(doctor.name)
+                return matcher.test(doctor.name);
+            })
+        },
+        filteredOrganization:function(){
+            let matcher = new RegExp(this.orgName,'i');
+            return this.organizations.filter(function(org){
+                return matcher.test(org.name); 
             })
         }
     },
     mounted(){
+        console.log("sdfdf")
         this.getAllDoctor();
+        this.getAllOrganization();
     }
 }
 </script>
