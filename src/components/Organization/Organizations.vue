8f677a59afb26aa4efa431b2b9b3f3db72b03d4e
kumol <kumol01763@gmail.com>
2021-02-14 23:13:46 +0600

    Routing issue fixed

--- src/components/Organization/Organizations.vue
+++ src/components/Organization/Organizations.vue
@@ -4,7 +4,6 @@
         <table class="table table-hover">
             <thead>
                 <tr>
-                    <th scope="col">Organization Id</th>
                     <th scope="col">Name</th>
                     <th scope="col">Email</th>
                     <th scope="col">Address</th>
@@ -13,8 +12,7 @@
             </thead>
             <tbody>
                 <tr v-for="organization in organizations" :key="organization._id">
-                    <th scope="row"><router-link class="nav-link nav-title" to="/organization/:organization._id">{{organization._Id}}</router-link></th>
-                    <td>{{organization.name}}</td>
+                    <th scope="row"><a :href="'/organization/' + organization._id" >{{organization.name}}</a></th>
                     <td>{{organization.email}}</td>
                     <td>{{organization.address}}</td>
                     <td><button @click="selectForDelete(organization._id)" class="btn btn-danger">Delete</button></td>
--- src/router/index.js
+++ src/router/index.js
@@ -31,7 +31,7 @@ const routes = [
     },
     {
       path: "/organization",
-      name: "Organization",
+      name: "All Organization",
       component: Organizations
     },
     {
