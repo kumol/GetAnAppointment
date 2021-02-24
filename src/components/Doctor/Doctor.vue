1d9ccac2109c59acfe34dc0aef9a2fb6f15bc5a6
kumol <kumol01763@gmail.com>
2021-02-24 23:49:17 +0600

    doctor profile

--- src/components/Doctor/Doctor.vue
+++ src/components/Doctor/Doctor.vue
@@ -2,15 +2,31 @@
     <div class="row m-0">
         <div class="col-md-3 col-sm-4 p-10">
             <div class="m-10 bg-white">
-                profile
+                <div style="width:100%;">
+                    <img style="width:100% !important;" src="https://clinicalnotebook.com/wp-content/uploads/2015/04/Doctor-Profile-Pic-Example.png" alt="">
+                </div>
+                <div class="p-10">
+                    <p class="m-0">{{doctor.name}}</p>
+                    <p class="m-0">Email</p>
+                    <hr class="hr"/>
+                    <p class="m-0"> {{doctor.email}} </p>
+                </div>
             </div>
             
         </div>
         <div class="col-md-9 col-sm-8 p-10">
-            <div class=" m-10 bg-white ">
-                details
+            <div class="row m-0">
+                    <div class="col-md-3 col-sm-3 bg-white p-0">col3</div>
+                    <div class="col-md-3 col-sm-3 bg-white p-0">col3</div>
+                    <div class="col-md-3 col-sm-3 bg-white p-0">col3</div>
+                    <div class="col-md-3 col-sm-3 bg-white p-0">col3</div>
+                </div>
+                <div class="row m-0 bg-white" style="margin-top:20px!important">
+                    hello
+                </div>
+            <div class="m-10 bg-white ">
+                
             </div>
-            
         </div>
     </div>
 </template>
